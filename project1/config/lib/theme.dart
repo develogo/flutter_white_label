22476c95421ca538deb2dfcba3694c9ae9e6f3bf
Felipe Andrade <lippeandrade@live.com>
2022-09-03 09:36:34 -0300

    Initial commit

---
+++ project1/config/lib/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const MaterialColor primary = MaterialColor(
+    _primaryValue,
+    <int, Color>{
+      50: Color.fromRGBO(_rValue, _gValue, _bValue, .1),
+      100: Color.fromRGBO(_rValue, _gValue, _bValue, .2),
+      200: Color.fromRGBO(_rValue, _gValue, _bValue, .3),
+      300: Color.fromRGBO(_rValue, _gValue, _bValue, .4),
+      400: Color.fromRGBO(_rValue, _gValue, _bValue, .5),
+      500: Color(_primaryValue),
+      600: Color.fromRGBO(_rValue, _gValue, _bValue, .7),
+      700: Color.fromRGBO(_rValue, _gValue, _bValue, .8),
+      800: Color.fromRGBO(_rValue, _gValue, _bValue, .9),
+      900: Color.fromRGBO(_rValue, _gValue, _bValue, 1),
+    },
+  );
+  static const int _primaryValue = 0xFF00FF13;
+  static const int _rValue = 0;
+  static const int _gValue = 255;
+  static const int _bValue = 19;
+}
---
+++ project2/config/lib/theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const MaterialColor primary = Colors.red;
+}
